428365e20a715d1beb9e86cfce026d6db0e4f1a9
X.LI <ralflea@gmail.com>
2021-01-10 12:44:06 -0800

    init version, not tested

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HappyMeal', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (burger_name) VALUES ('Fish');
