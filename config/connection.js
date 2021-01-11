b0212c1ceb3cce39edd755e2b25b8d096899af9b
X.LI <ralflea@gmail.com>
2021-01-10 17:00:34 -0800

    minor update

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-const dburl = process.env.JAWSDB_URL 
-            || {
-                host: 'localhost',
-                port: 3306,
-                user: "root",
-                password: "password",
-                database: "burger_db"
-              }
-var connection = mysql.createConnection(dburl);
+const dburl = process.env.JAWSDB_URL; 
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection(
+        {
+            host: 'localhost',
+            port: 3306,
+            user: "root",
+            password: "password",
+            database: "burger_db"
+          }
+    )
+}
 
 // Make connection.
 connection.connect(function(err) {
